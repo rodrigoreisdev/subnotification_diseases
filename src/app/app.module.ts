67deac5e10c0bfa382a582043eedbc6bbea92897
Asgator7 <antonioalves055@hotmail.com>
2022-05-09 00:30:30 -0300

    criação - navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NavbarComponent } from './components/commom/navbar/navbar.component';
+import { FooterComponent } from './components/commom/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
